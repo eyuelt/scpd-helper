5a6ca71a62284cc2b8359e84f2b40642671e9be9
eyuelt <eyueltessema@gmail.com>
2013-05-12 16:37:50 -0700

    Modify description comment in scrape.py

--- scrape.py
+++ scrape.py
@@ -13,7 +13,8 @@ that they happened as a wmv, then converts them to a mp4. Each time
 the script is run, it will update to download all of the undownloaded
 videos. 
 
-This script is modified from the one by Ben Newhouse (https://github.com/newhouseb).
+This script is modified from the one by Jeremy Keeshin (https://github.com/jkeesh),
+which itself was modified from the one by Ben Newhouse (https://github.com/newhouseb).
 
 Unfortunately, there are lots of dependencies to get it up and running
 1. Handbrake CLI, for converting to mp4: http://handbrake.fr/downloads2.php
@@ -23,7 +24,7 @@ Unfortunately, there are lots of dependencies to get it up and running
 Usage: python scrape.py [Stanford ID] "Interactive Computer Graphics"
 
 The way I use it is to keep a folder of videos, and once I have watched them, move them
-into a subfolder called watched. So it also wont redowload files that are in a subfolder
+into a subfolder called watched. So it also wont redownload files that are in a subfolder
 called watched.
 
 

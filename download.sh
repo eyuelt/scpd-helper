86d58e8f0a968fa01a7096d911e31046cb692f3e
eyuelt <eyueltessema@gmail.com>
2014-10-24 01:42:53 -0700

    Don't exit until done. Other fixes

--- download.sh
+++ download.sh
@@ -1,18 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
+# $1 is url and $2 is destination path
+start_download() {
+    echo "Getting $2"
+    tmpfilename="$2.tmp"
+    curl -s $1 > $2
+    mv $tmpfilename $2
+    echo "Finished downloading $2"
+}
 
 if [[ -n $1 ]]
 then
     while read line
     do
-        if [[ $line =~ .*stanford.edu/videos/courses/.*\.mp4$ ]]
+        if [[ $line =~ .*\.mp4$ ]]
         then
             filename=$(echo $line | rev | sed s/\\/\.\*$// | rev)
-            echo "Downloading video: '"$line"' and storing it in file '"$filename"'"
-            curl -s $line > $filename &
+            if [[ -a $filename ]]
+            then
+                echo "This file already exists: $filename"
+            else
+                start_download $line $filename &
+            fi
         else
-            echo "Invalid myvideosu link: "$line
+            echo "Invalid link: "$line
         fi
     done < $1
+    wait
 else
     echo "Usage: "$0" [link_file]"
 fi
